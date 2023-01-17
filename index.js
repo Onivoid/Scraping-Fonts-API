dc7b38b441393210caaef95a772b63fa18d65ca2
Logan Wilquin <Skullyfox.yt@gmail.com>
2023-01-17 14:49:31 +0100

    CORS again ....

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, custom-header");
   next();
 });
 
